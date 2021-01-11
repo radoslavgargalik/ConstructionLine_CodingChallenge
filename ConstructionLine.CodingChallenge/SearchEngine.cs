4a0a90e7e1e40bfecc1748f0d7189f94915e4178
Radoslav Gargalik <radoslav.gargalik@gmail.com>
2021-01-11 07:54:08 +0000

    Implementation of SearchEngine

--- ConstructionLine.CodingChallenge/SearchEngine.cs
+++ ConstructionLine.CodingChallenge/SearchEngine.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
 
 namespace ConstructionLine.CodingChallenge
 {
@@ -9,18 +11,41 @@ namespace ConstructionLine.CodingChallenge
         public SearchEngine(List<Shirt> shirts)
         {
             _shirts = shirts;
-
-            // TODO: data preparation and initialisation of additional data structures to improve performance goes here.
-
         }
-
 
         public SearchResults Search(SearchOptions options)
         {
-            // TODO: search logic goes here.
+            if (options?.Colors == null || options.Sizes == null)
+                throw new ArgumentException("SearchOptions and its collections must not be null", nameof(options));
 
+            var colorsToSearch = options.Colors.ToHashSet();
+            var sizesToSearch = options.Sizes.ToHashSet();
+
+            var matchedShirts = _shirts
+                .Where(s =>
+                    (!colorsToSearch.Any() || colorsToSearch.Contains(s.Color)) &&
+                    (!sizesToSearch.Any() || sizesToSearch.Contains(s.Size)))
+                .ToList();
+
+            var colorCounts = matchedShirts
+                .GroupBy(s => s.Color)
+                .Select(g => new ColorCount {Color = g.Key, Count = g.Count()});
+            var missingColorCounts = Color.All
+                .Where(c => !colorCounts.Any(cc => cc.Color == c))
+                .Select(c => new ColorCount {Color = c, Count = 0});
+
+            var sizeCounts = matchedShirts
+                .GroupBy(s => s.Size)
+                .Select(g => new SizeCount {Size = g.Key, Count = g.Count()});
+            var missingSizeCounts = Size.All
+                .Where(s => !sizeCounts.Any(sc => sc.Size == s))
+                .Select(s => new SizeCount {Size = s, Count = 0});
+            
             return new SearchResults
             {
+                Shirts = matchedShirts,
+                ColorCounts = colorCounts.Union(missingColorCounts).ToList(),
+                SizeCounts = sizeCounts.Union(missingSizeCounts).ToList()
             };
         }
     }
